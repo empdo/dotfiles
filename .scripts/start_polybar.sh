fc31c17b2e97218837fbc05f51d9d445b207656b
Emil Essung <emil.essung@live.se>
2021-03-23 19:46:53 +0100

    add media buttons

--- .scripts/start_polybar.sh
+++ .scripts/start_polybar.sh
@@ -9,5 +9,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar dp0 &
 polybar dp1 &
+polybar dp2 &
 
 echo "Polybar launched..."
